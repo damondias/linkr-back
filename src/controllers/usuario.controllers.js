e8c84ffee5ffce41036999a5337d2dd3e2fac18e
Pedro Oriani <pedro.oriani@gmail.com>
2023-08-20 12:56:02 -0300

    refactor: alteração das tabelas e colunas user e sessions

--- src/controllers/usuario.controllers.js
+++ src/controllers/usuario.controllers.js
@@ -3,19 +3,19 @@ import bcrypt from 'bcrypt';
 import { v4 as uuid } from 'uuid';
 
 export async function cadastro(req, res) {
-    const { email, senha, username, url } = req.body;
+    const { email, password, username, image } = req.body;
 
     try {
-        const emailValidate = await db.query('SELECT * FROM cadastro WHERE email = $1;', [email]);
+        const emailValidate = await db.query('SELECT * FROM users WHERE email = $1;', [email]);
         if (emailValidate.rows.length !== 0) {
             return res.status(409).send("email inválido");
         }
 
-        const bcryptSenha = bcrypt.hashSync(senha, 10);
+        const bcryptpassword = bcrypt.hashSync(password, 10);
 
         const cadastro = await db.query(
-            'INSERT INTO cadastro (email, senha, username, url) VALUES ($1, $2, $3, $4) RETURNING id;',
-            [email, bcryptSenha, username, url]
+            'INSERT INTO users (email, password, username, image) VALUES ($1, $2, $3, $4) RETURNING id;',
+            [email, bcryptpassword, username, image]
         );
 
         console.log("Cadastro inserido com sucesso:", cadastro);
@@ -23,12 +23,12 @@ export async function cadastro(req, res) {
 
         // const cadastroId = cadastro.rows[0].id;
 
-        const usuario = await db.query(
-            'INSERT INTO login (email, senha) VALUES ($1, $2);',
-            [ email, bcryptSenha]
-        );
+        // const usuario = await db.query(
+        //     'INSERT INTO session (email, password) VALUES ($1, $2);',
+        //     [ email, bcryptpassword]
+        // );
 
-        console.log("Usuário inserido com sucesso:", usuario);
+        // console.log("Usuário inserido com sucesso:", usuario);
 
 
         res.sendStatus(201);
@@ -40,11 +40,11 @@ export async function cadastro(req, res) {
 
 export async function login(req, res){
 
-    const {email, senha} = req.body
+    const {email, password} = req.body
 
     try{
 
-        const verificadorLogin = await db.query(`SELECT * FROM login WHERE email = $1`, [email])
+        const verificadorLogin = await db.query(`SELECT * FROM sessions WHERE email = $1`, [email])
 
         if (verificadorLogin.rowCount === 0) {
             return res.sendStatus(401);
@@ -53,15 +53,15 @@ export async function login(req, res){
         const user = verificadorLogin.rows[0];
 
 
-        const senhaCorreta = await bcrypt.compare(senha, user.senha);
+        const passwordCorreta = await bcrypt.compare(password, user.password);
 
-        if (!senhaCorreta) {
-            return res.status(401).send("E-mail ou senha inválido");
+        if (!passwordCorreta) {
+            return res.status(401).send("E-mail ou password inválido");
         }
 
         const token = uuid();
 
-        const login = await db.query('INSERT INTO tokens (user_id, token) VALUES ($1, $2);', [user.id, token])
+        const login = await db.query('INSERT INTO sessions (user_id, token) VALUES ($1, $2);', [user.id, token])
 
         res.status(200).send({token})
 
--- src/schemma/usuario.schemma.js
+++ src/schemma/usuario.schemma.js
@@ -3,12 +3,12 @@ import Joi from "joi"
 
 export const cadastroSchema = Joi.object({
     email: Joi.string().email().required(),
-    senha: Joi.string().min(3).required(),
+    password: Joi.string().min(3).required(),
     username: Joi.string().required(),
-    url: Joi.string()
+    image: Joi.string()
 });
 
 export const loginSchemma = Joi.object({
     email: Joi.string().email().required(),
-    senha: Joi.string().min(3).required()
+    password: Joi.string().min(3).required()
 })
