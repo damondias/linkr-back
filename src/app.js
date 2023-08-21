cd40856d357c750cde5f19c11175cfcb0b2085b6
Damon <damondias@gmail.com>
2023-08-21 15:21:25 -0400

    console log of app.js

--- src/app.js
+++ src/app.js
@@ -17,4 +17,8 @@ app.use(router)
 
 //  Deixa o app escutando, à espera de requisições
 const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
+app.listen(PORT, () => console.log(`    
+    Running API Linkr on port ${PORT},
+    Url: http://localhost:${PORT}
+
+`))
