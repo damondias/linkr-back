900e381fd382056db5bb6b1d3507e02563cc793c
Damon <damondias@gmail.com>
2023-08-25 15:55:04 -0400

    feat: scroll infinite in userPage and hashtagpage

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,10 +1,13 @@
 import { getUserIdByName, getUserPosts } from "../repositories/users.repository.js"
 
 export async function getUserById(req,res){
-    const {id} = req.params
+    const {id} = req.params;
+
+    const { limit } = req.query;
+    const { offset } = req.query;
 
     try {
-        const {rows} = await getUserPosts(id)
+        const {rows} = await getUserPosts(id,limit, offset)
         if(rows.length == 0) return res.status(404).send("User not found")
         res.status(200).send(rows)
     } catch (err) {
