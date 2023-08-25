6140227da9b6e73db8236b018bff75cfdd92fbd0
Pedro Oriani <pedro.oriani@gmail.com>
2023-08-25 02:41:05 -0300

    fix: comments

--- src/controllers/comment.controllers.js
+++ src/controllers/comment.controllers.js
@@ -13,7 +13,7 @@ export async function getComments (req, res){
         const count = await queryCountComments(postId);
         const following = await queryIsFollowing (userId)
 
-        res.send({countComment: count, comments: comments.rows, following: following})
+        res.send({countComment: count.rowCount, comments: comments.rows, following: following.rows})
 
     }catch (err){
         res.status(500).send(err.message)
